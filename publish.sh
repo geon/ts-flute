ed3b57499ddbde25e04373efa9acacd0f93cad86
Victor Widell <victor.widell@gmail.com>
2024-11-19 08:19:18 +0100

    Added publish script.

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# TODO: add --no-tags
+git fetch origin gh-pages:gh-pages
+git clone --local --shared --no-checkout --single-branch --branch gh-pages . temp-gh-pages-repo
+cd temp-gh-pages-repo
+git --work-tree=../dist add .
+git --work-tree=../dist commit -m "Published"
+git push
+chmod -R +w .git
+cd ..
+rm -r temp-gh-pages-repo
+git push origin gh-pages
