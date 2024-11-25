a31ed95c660fefb022fd2fe2d874f5c960c075ec
Victor Widell <victor.widell@gmail.com>
2024-11-25 18:07:13 +0100

    Refactor. Moved GlobalStyles to own file.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Root } from "./ui/Root.tsx";
-import { createGlobalStyle } from "styled-components";
 import { Reset } from "styled-reset";
-
-const GlobalStyles = createGlobalStyle`
-  html {
-  }
-`;
+import { GlobalStyles } from "./ui/GlobalStyles.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
