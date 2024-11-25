6f01429383ea08032e5c7fa4aa74f2fbf5e0d7b8
Victor Widell <victor.widell@gmail.com>
2024-11-25 09:14:30 +0100

    Fixed accidental selections.

--- src/ui/GlobalStyles.tsx
+++ src/ui/GlobalStyles.tsx
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  html {
-  }
+	html {
+		/* Prevent dragging and long pressing from selecting on mobile Safari. */
+		user-select: none;
+		-webkit-user-select: none;
+	}
 `;
