3964fb9b8422f3efb0ac7a1e4eb4f216b0871494
Victor Widell <victor.widell@gmail.com>
2024-11-25 19:59:51 +0100

    Fixed scrolling and bouncing on mobile.

--- src/ui/GlobalStyles.tsx
+++ src/ui/GlobalStyles.tsx
@@ -5,5 +5,8 @@ export const GlobalStyles = createGlobalStyle`
 		/* Prevent dragging and long pressing from selecting on mobile Safari. */
 		user-select: none;
 		-webkit-user-select: none;
+
+		/* Prevents scrolling and bounce on mobile Safari. */
+		overflow: hidden;
 	}
 `;
